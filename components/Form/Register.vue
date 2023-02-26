2c6def1492412a77f86d5c96712ce17ee20abc27
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-26 10:32:56 -0300

    chore: remove formkit

--- components/Form/Register.vue
+++ components/Form/Register.vue
@@ -13,41 +13,6 @@ const handleRegister = () => {
 
 <template>
   <div class="flex flex-col">
-    <FormKit type="form" submit-label="Criar conta" @submit="handleRegister">
-      <h1 class="text-xl mb-4">Criar conta</h1>
-      <FormKit
-        v-model="data.username"
-        type="text"
-        name="username"
-        id="username"
-        label="Username"
-        help="Your username"
-        placeholder="@username"
-        validation="required"
-      />
-
-      <FormKit
-        v-model="data.password"
-        type="password"
-        name="password"
-        id="password"
-        label="Password"
-        help="Your password"
-        placeholder="********"
-        validation="required"
-      />
-
-      <FormKit
-        v-model="data.confirmPassword"
-        type="password"
-        name="confirmPassword"
-        id="confirmPassword"
-        label="Confirm password"
-        help="Confirm your password"
-        placeholder="********"
-        validation="required"
-      />
-    </FormKit>
-    <nuxt-link to="/login">Login</nuxt-link>
+    <h1 class="text-xl mb-4">Criar conta</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@formkit/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
   typescript: {
     strict: true,
   },
