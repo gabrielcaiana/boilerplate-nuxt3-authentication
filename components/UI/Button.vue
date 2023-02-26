8fc2348941037ebbf9b91d9082293c56ebc1094b
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-26 13:09:16 -0300

    feat: create login page

---
+++ components/UI/Button.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+interface Props {
+  label: string;
+  color?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  label: 'hello',
+});
+
+const emit = defineEmits<{
+  (e: 'click'): void;
+}>();
+
+const handleClick = () => {
+  emit('click');
+};
+
+const btnClasses = computed(() => {
+  if (props.color === 'secondary') {
+    return 'border-blue-500 hover:bg-blue-500 text-blue-500 hover:text-white';
+  }
+
+  return 'bg-blue-500 hover:bg-blue-700 text-white';
+});
+</script>
+
+<template>
+  <button
+    @click="handleClick"
+    :class="[
+      'border transition-colors py-3 font-medium rounded-md',
+      btnClasses,
+    ]"
+  >
+    {{ label }}
+  </button>
+</template>
--- pages/login.vue
+++ pages/login.vue
@@ -2,10 +2,56 @@
 useHead({
   title: 'Login',
 });
+
+const router = useRouter();
+
+const data = reactive({
+  username: '',
+  password: '',
+});
+
+const handleLogin = () => {
+  alert('hello world');
+};
+
+const handleRegisterPage = () => {
+  router.push('/register');
+};
 </script>
 
 <template>
   <div class="container mx-auto p-10 h-full flex items-center justify-center">
-    <FormLogin />
+    <VForm @submit="handleLogin" class="flex flex-col gap-4 p-10 w-2/5">
+      <h1 class="text-2xl font-medium text-gray-700">Faça Login</h1>
+      <div>
+        <VField
+          type="text"
+          name="username"
+          rules="required"
+          placeholder="Enter username"
+          class="block border border-gray-400 p-2 rounded-md w-full"
+        />
+        <VErrorMessage name="username" class="text-red-500" />
+      </div>
+
+      <div>
+        <VField
+          type="password"
+          name="password"
+          rules="required"
+          placeholder="Enter password"
+          class="block border border-gray-400 p-2 rounded-md w-full"
+        />
+        <VErrorMessage name="password" class="text-red-500" />
+      </div>
+
+      <UIButton label="Login" type="submit" />
+      <UIButton
+        label="Cadastre-se"
+        type="button"
+        color="secondary"
+        @click="handleRegisterPage"
+      />
+    </VForm>
   </div>
 </template>
