938509f072b1aec1093de1f760f220c0de8f0a35
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-26 01:53:23 -0300

    chore: add button to change pages

--- components/Form/Login.vue
+++ components/Form/Login.vue
@@ -11,28 +11,31 @@ const handleLogin = () => {
 </script>
 
 <template>
-  <FormKit type="form" submit-label="Login" @submit="handleLogin">
-    <h1 class="text-xl mb-4">Login</h1>
-    <FormKit
-      v-model="data.username"
-      type="text"
-      name="username"
-      id="username"
-      label="Username"
-      help="Your username"
-      placeholder="@username"
-      validation="required"
-    />
+  <div class="flex flex-col">
+    <FormKit type="form" submit-label="Login" @submit="handleLogin">
+      <h1 class="text-xl mb-4">Login</h1>
+      <FormKit
+        v-model="data.username"
+        type="text"
+        name="username"
+        id="username"
+        label="Username"
+        help="Your username"
+        placeholder="@username"
+        validation="required"
+      />
 
-    <FormKit
-      v-model="data.password"
-      type="password"
-      name="password"
-      id="password"
-      label="Password"
-      help="Your password"
-      placeholder="********"
-      validation="required"
-    />
-  </FormKit>
+      <FormKit
+        v-model="data.password"
+        type="password"
+        name="password"
+        id="password"
+        label="Password"
+        help="Your password"
+        placeholder="********"
+        validation="required"
+      />
+    </FormKit>
+    <nuxt-link to="/register">Criar conta</nuxt-link>
+  </div>
 </template>
--- components/Form/Register.vue
+++ components/Form/Register.vue
@@ -12,39 +12,42 @@ const handleRegister = () => {
 </script>
 
 <template>
-  <FormKit type="form" submit-label="Criar conta" @submit="handleRegister">
-    <h1 class="text-xl mb-4">Criar conta</h1>
-    <FormKit
-      v-model="data.username"
-      type="text"
-      name="username"
-      id="username"
-      label="Username"
-      help="Your username"
-      placeholder="@username"
-      validation="required"
-    />
+  <div class="flex flex-col">
+    <FormKit type="form" submit-label="Criar conta" @submit="handleRegister">
+      <h1 class="text-xl mb-4">Criar conta</h1>
+      <FormKit
+        v-model="data.username"
+        type="text"
+        name="username"
+        id="username"
+        label="Username"
+        help="Your username"
+        placeholder="@username"
+        validation="required"
+      />
 
-    <FormKit
-      v-model="data.password"
-      type="password"
-      name="password"
-      id="password"
-      label="Password"
-      help="Your password"
-      placeholder="********"
-      validation="required"
-    />
+      <FormKit
+        v-model="data.password"
+        type="password"
+        name="password"
+        id="password"
+        label="Password"
+        help="Your password"
+        placeholder="********"
+        validation="required"
+      />
 
-    <FormKit
-      v-model="data.confirmPassword"
-      type="password"
-      name="confirmPassword"
-      id="confirmPassword"
-      label="Confirm password"
-      help="Confirm your password"
-      placeholder="********"
-      validation="required"
-    />
-  </FormKit>
+      <FormKit
+        v-model="data.confirmPassword"
+        type="password"
+        name="confirmPassword"
+        id="confirmPassword"
+        label="Confirm password"
+        help="Confirm your password"
+        placeholder="********"
+        validation="required"
+      />
+    </FormKit>
+    <nuxt-link to="/login">Login</nuxt-link>
+  </div>
 </template>
