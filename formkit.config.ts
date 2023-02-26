4378d14aaf1e0ccfabb0be3e589f8795c8744b87
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-26 01:21:17 -0300

    chore: change formkit to pt

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,7 +1,12 @@
+import { pt } from '@formkit/i18n';
 import { DefaultConfigOptions } from '@formkit/vue';
 
 const config: DefaultConfigOptions = {
   theme: 'genesis',
+  locales: {
+    pt,
+  },
+  locale: 'pt',
 };
 
 export default config;
